a2dc29569238e5891a04633f7569867e15789308
Levin_Ihor <lytghzys@gmail.com>
2023-11-20 19:08:07 +0200

    Add tailwind.css and required dependencies

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import "./assets/base.css";
-
+import "./assets/tailwind.css";
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ["./index.html", "./src/**/*.vue"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
