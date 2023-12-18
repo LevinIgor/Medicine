3d814652e958019937a5a398f17db5f91551fbdc
Levin_Ihor <lytghzys@gmail.com>
2023-12-18 14:40:07 +0200

    Add base layout component

---
+++ src/layouts/base.vue
@@ -0,0 +1,30 @@
+<script setup>
+  import Breadcrumb from "@/components/Breadcrumb.vue";
+  import Appointment from "@/components/Appointment.vue";
+
+  const props = defineProps({
+    // [{name: 'Home', link: '/'}]
+    breadcrumb: {
+      type: Array,
+      required: true,
+    },
+
+    title: {
+      type: String,
+      required: true,
+    },
+  });
+</script>
+<template>
+  <main class="bg-gray">
+    <section class="container flex flex-col">
+      <Breadcrumb class="my-8" :breadcrumb="breadcrumb" />
+      <h2>{{ title }}</h2>
+      <slot />
+
+      <Appointment class="my-24" />
+    </section>
+  </main>
+</template>
+
+<style lang="scss" scoped></style>
