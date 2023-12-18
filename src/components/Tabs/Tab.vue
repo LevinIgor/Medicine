53b3f27805181202589eff35b1fd7ce5a28b82b2
Levin_Ihor <lytghzys@gmail.com>
2023-12-18 14:40:17 +0200

    Add Tab and Tabs components

---
+++ src/components/Tabs/Tab.vue
@@ -0,0 +1,18 @@
+<script setup>
+  import { inject, computed } from "vue";
+
+  const props = defineProps({
+    title: String,
+  });
+
+  const isActive = computed(() => {
+    return inject("activeTab").value === props.title;
+  });
+</script>
+<template>
+  <div v-show="isActive">
+    <slot></slot>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/components/Tabs/Tabs.vue
@@ -0,0 +1,80 @@
+<script setup>
+  import { ref, useSlots, provide, defineAsyncComponent } from "vue";
+
+  const slots = useSlots();
+  const props = defineProps({
+    isVertical: {
+      type: Boolean,
+      default: true,
+    },
+  });
+
+  const titles = ref(slots.default().map(slot => slot.props.title));
+  const icons = ref(slots.default().map(slot => slot.props.icon));
+
+  const activeTab = ref(titles.value[0]);
+  provide("activeTab", activeTab);
+
+  function getIcon(title) {
+    const index = titles.value.indexOf(title);
+    return defineAsyncComponent({
+      loader: () => import(`@/components/icons/${icons.value[index]}.vue`),
+    });
+  }
+</script>
+<template>
+  <div class="grid grid-cols-12 gap-5" v-if="isVertical">
+    <ul class="flex flex-col gap-5 col-span-3">
+      <li
+        class="cursor-pointer flex items-center gap-2 py-3 px-4 rounded-md bg-white text-gray-160 font-roboto select-none"
+        :class="{ active: activeTab === title }"
+        v-for="(title, index) in titles"
+        :key="index"
+        @click="(activeTab = title), $router.push({ params: { tab: title } })"
+      >
+        <component v-if="title.icon" :is="getIcon(title)" class="w-5 h-5" />
+        {{ title }}
+      </li>
+    </ul>
+
+    <div class="col-span-9">
+      <slot></slot>
+    </div>
+  </div>
+  <div class="flex flex-col" v-else>
+    <ul class="w-full flex items-center justify-between gap-5">
+      <li
+        class="w-full cursor-pointer py-3 rounded-md bg-white text-gray-160 font-roboto select-none text-center"
+        :class="{ active: activeTab === title }"
+        v-for="(title, index) in titles"
+        :key="index"
+        @click="(activeTab = title), $router.push({ params: { tab: title } })"
+      >
+        {{ title }}
+      </li>
+    </ul>
+
+    <div class="mt-5 bg-white py-10 px-6 rounded-md">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  ul {
+    padding: 0;
+    li:hover {
+      background-color: var(--blue-180);
+      color: #fff;
+      transition:
+        background-color 0.3s ease-in-out,
+        color 0.3s ease-in-out;
+    }
+    .active {
+      background-color: var(--blue-180);
+      color: #fff;
+      font-weight: 600;
+      font-style: normal;
+    }
+  }
+</style>
