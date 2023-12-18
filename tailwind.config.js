924a32984999d8b5a1739c6fe3818346f9687b39
Levin_Ihor <lytghzys@gmail.com>
2023-12-18 14:36:57 +0200

    Add new CSS files and update base.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,37 @@
 module.exports = {
-  purge: ["./index.html", "./src/**/*.vue"],
-  darkMode: false, // or 'media' or 'class'
+  content: ["./index.html", "./src/**/*.vue"],
   theme: {
-    extend: {},
+    colors: {
+      gray: "#f7f8f9",
+      white: "#ffffff",
+      black: "#000000",
+      yellow: "#ffc700",
+      red: "#ff3f3f",
+      green: "#69da57",
+      "gray-100": "#d7d7d7",
+      "gray-140": "#868c9b",
+      "gray-160": "#656d7e",
+      "gray-180": "#3e4958",
+      "gray-200": "#1a2433",
+      "blue-160": "#eaf4ff",
+      "blue-180": "#3d86f5",
+      "blue-200": "#3d86f5",
+      "violet-180": "#f3eefd",
+      "violet-200": "#9975e2",
+      "orange-180": "#fff7f0",
+      "orange-200": "#e9ae7a",
+    },
+    fontFamily: {
+      montserrat: ["Montserrat", "sans-serif"],
+      roboto: ["Inter", "sans-serif"],
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+    },
+    screens: {
+      md: { max: "767px" },
+    },
   },
   variants: {
     extend: {},
